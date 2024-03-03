7287ab03ca1acb834bda77921c7642c8913acbd3
lunacomp <vrien87@gmail.com>
2024-03-03 14:08:47 +0700

    Update earnfm-pstream.sh

--- earnfm-pstream.sh
+++ earnfm-pstream.sh
@@ -1,7 +1,7 @@
-apt-get update
-apt-get upgrade -y
- curl -fsSL https://get.docker.com -o get-docker.sh
- sudo sh get-docker.sh
+#apt-get update
+#apt-get upgrade -y
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sudo sh get-docker.sh
 # EARNFM:
 sudo docker stop watchtower
 sudo docker rm watchtower
@@ -9,8 +9,8 @@ sudo docker rmi containrrr/watchtower
 sudo docker stop earnfm-client
 sudo docker rm earnfm-client
 sudo docker rmi earnfm/earnfm-client:latest
-sudo docker run -d --restart=always -e EARNFM_TOKEN="ae5f4832-ce33-4156-959d-0cee6654d0d0" --name earnfm-client earnfm/earnfm-client:latest
-sudo docker run -d --restart=always --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --include-restarting --revive-stopped --interval 60 earnfm-client
+#sudo docker run -d --restart=always -e EARNFM_TOKEN="ae5f4832-ce33-4156-959d-0cee6654d0d0" --name earnfm-client earnfm/earnfm-client:latest
+#sudo docker run -d --restart=always --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --include-restarting --revive-stopped --interval 60 earnfm-client
 
 #PSTREAM:
 sudo docker stop watchtower
@@ -19,6 +19,6 @@ sudo docker rmi containrrr/watchtower
 sudo docker stop psclient
 sudo docker rm psclient
 sudo docker rmi packetstream/psclient
-sudo docker run -d --restart=always -e CID=6CIE --name psclient packetstream/psclient:latest
-sudo docker run -d --restart=always --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --include-restarting --revive-stopped --interval 60 psclient
+#sudo docker run -d --restart=always -e CID=6CIE --name psclient packetstream/psclient:latest
+#sudo docker run -d --restart=always --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --include-restarting --revive-stopped --interval 60 psclient
 
