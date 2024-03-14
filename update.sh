bf907a4eeea4cf38f4ee06016e1f611ffb983712
lunacomp <vrien87@gmail.com>
2024-03-15 05:17:40 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Fungsi untuk memeriksa koneksi internet
-check_internet_connection() {
+check_internet() {
     wget -q --spider http://google.com
 
     if [ $? -eq 0 ]; then
-        echo "Koneksi internet terdeteksi."
+        echo "Internet terhubung."
         return 0
     else
         echo "Tidak ada koneksi internet."
@@ -13,21 +13,33 @@ check_internet_connection() {
     fi
 }
 
-# Memeriksa koneksi internet
-if check_internet_connection; then
-    # Jika koneksi internet tersedia, jalankan perintah apt-get update dan apt-get upgrade
-    echo "Memperbarui daftar paket..."
-sleep 4
- clear
-    apt-get update
+# Loop sampai koneksi internet terhubung
+while ! check_internet; do
+    echo "Menunggu koneksi internet..."
+    sleep 5
+done
 
-    echo "Meng-upgrade paket..."
-sleep 4
- clear
-    apt-get upgrade -y
-     apt autoremove
-     apt autoclean
-else
-    echo "Gagal memeriksa koneksi internet. Proses dihentikan."
-fi
-
+# Jika koneksi internet terhubung, lanjutkan dengan update, upgrade, autoremove, dan autoclean
+sleep 3
+  clear
+echo "Melakukan update paket..."
+sleep 5
+  clear
+apt-get update
+  clear
+echo "Melakukan upgrade paket..."
+sleep 5
+  clear
+apt-get upgrade -y
+  clear
+echo "Melakukan autoremove..."
+sleep 5
+  clear
+apt autoremove -y
+  clear
+echo "Melakukan autoclean..."
+sleep 5
+  clear
+apt autoclean
+  clear
+echo "Selesai."
