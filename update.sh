f64f6ce8ac6c722696b08b97484cf4c177b7bf23
lunacomp <vrien87@gmail.com>
2024-03-14 14:22:53 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,31 @@
-apt-get update
+#!/bin/bash
+
+# Fungsi untuk memeriksa koneksi internet
+check_internet_connection() {
+    wget -q --spider http://example.com
+
+    if [ $? -eq 0 ]; then
+        echo "Koneksi internet terdeteksi."
+        return 0
+    else
+        echo "Tidak ada koneksi internet."
+        return 1
+    fi
+}
+
+# Memeriksa koneksi internet
+if check_internet_connection; then
+    # Jika koneksi internet tersedia, jalankan perintah apt-get update dan apt-get upgrade
+    echo "Memperbarui daftar paket..."
+    apt-get update
+
+    echo "Meng-upgrade paket..."
     apt-get upgrade -y
-    #armbian-update
-    apt autoremove
+     apt autoremove
+     apt autoclean
+else
+    echo "Gagal memeriksa koneksi internet. Proses dihentikan."
+fi
+
+   
 apt autoclean
