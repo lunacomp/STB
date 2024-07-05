9b3464e989d7b4fbccf1d0fbd543d9d9274e179a
lunacomp <vrien87@gmail.com>
2024-07-05 08:06:16 +0700

    Update re-install-earnapp.sh

--- re-install-earnapp.sh
+++ re-install-earnapp.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# Uninstall EarnApp
-echo "Uninstalling EarnApp..."
-echo "y" | earnapp uninstall
-
-# Confirm second prompt
-echo "y" | earnapp confirm
-
-echo "EarnApp has been uninstalled successfully."
-
 # Download and install EarnApp
 echo "Downloading and installing EarnApp..."
 wget -qO- https://brightdata.com/static/earnapp/install.sh > /tmp/earnapp.sh
